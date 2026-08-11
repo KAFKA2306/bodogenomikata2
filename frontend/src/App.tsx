b86bcbb098c2a43029b0b9083c7028c787b42115
KAFKA <137051370+KAFKA2306@users.noreply.github.com>
2026-08-11 14:56:02 +0900

    feat: route Palworld card database

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { HomePage } from './pages/HomePage';
 import { PlayModePage } from './pages/PlayModePage';
 import { ComparisonPage } from './pages/ComparisonPage';
 import { GameDetailPage } from './pages/GameDetailPage';
+import { PalworldCardSearchPage } from './pages/PalworldCardSearchPage';
 import { ProductShell } from './components/ProductShell';
 import { KafkaSignalRuntime } from './components/KafkaSignalRuntime';
 import './index.css';
@@ -20,6 +21,7 @@ function App() {
           <Route path="/play" element={<PlayModePage intent="play" />} />
           <Route path="/install" element={<PlayModePage intent="install" />} />
           <Route path="/research" element={<ProductShell><ComparisonPage /></ProductShell>} />
+          <Route path="/cards/palworld" element={<ProductShell><PalworldCardSearchPage /></ProductShell>} />
           <Route path="/game/:slug" element={<ProductShell><GameDetailPage /></ProductShell>} />
         </Routes>
       </BrowserRouter>
